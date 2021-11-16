12d3c500f7ca7702ddcf289a224eb4ad5898f4d7
DylanLebatteux <67757611+DylanLebatteux@users.noreply.github.com>
2021-11-16 11:52:23 -0500

    Delete Objective.py

--- Objective.py
+++
@@ -1,7 +0,0 @@
-# Function checking objective
-def checkObjective(objective_score, scores):
-	objective = False
-	for s in scores:
-		if s[0] >= objective_score and s[1] >= objective_score:
-			objective = True
-	return objective
