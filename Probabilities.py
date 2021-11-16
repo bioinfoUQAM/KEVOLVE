88dce10aec4263430a60af9985bb9dc62ae5af30
DylanLebatteux <67757611+DylanLebatteux@users.noreply.github.com>
2021-11-16 11:52:40 -0500

    Delete Probabilities.py

--- Probabilities.py
+++
@@ -1,5 +0,0 @@
-# # Function updating probabilities of genes to be selected
-def calculProbabilities(weights):
-	probabilities = []
-	for i in weights: probabilities.append(i * (1 / sum(weights)))
-	return probabilities
